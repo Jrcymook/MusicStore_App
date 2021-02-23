166475f5de6bfb8a5c85cbf411f109413a0ff070
Jrcymook <55121062+Jrcymook@users.noreply.github.com>
2021-02-24 01:43:56 +0700

    Update musicinfo.dart

--- lib/musicinfo.dart
+++ lib/musicinfo.dart
@@ -1,61 +1,63 @@
 const MUSIC_INFO = [
   {
-    "name":"0-Supercuts",
+    "name":"Supercuts",
     "brand":"Jeremy Zucker",
     "price":2.99,
     "image":"jeremysuppercuts.jpg"
   },{
-    "name":"1-Visions of gideon",
+    "name":"Visions of gideon",
     "brand":"Sufjan Stevens",
     "price":4.99,
     "image":"sufjanvisionsofgideon.png"
   },
   {
-    "name":"2-Beautiful People",
+    "name":"Beautiful People",
     "brand":"Ed Sheeran, Khalid",
     "price":3.49,
     "image":"beautifulpeopleed.png"
   },
   {
-    "name":"3-Steal My Girl",
+    "name":"Steal My Girl",
     "brand":"One Direction",
     "price":2.99,
     "image":"onedirectionstealmygirl.jpg"
   },
   {
-    "name":"4-Come Around Me",
+    "name":"Come Around Me",
     "brand":"Justin Bieber",
     "price":9.49,
     "image":"justinComearoundme.png"
   },
   {
-    "name":"5-Daylight",
+    "name":"Daylight",
     "brand":"Taylor Swift",
     "price":5.49,
     "image":"taylordaylight.jpg"
   },
   {
-    "name":"6-What Lovers Do",
+    "name":"What Lovers Do",
     "brand":"Maroon5, SZE",
     "price":17.99,
     "image":"maroon5whatlovedo.jpg"
   },
   {
-    "name":"7-Wild",
+    "name":"Wild",
     "brand":"Troye Sivan",
     "price":3.99,
     "image":"troyewild.jpg"
   },
   {
-    "name":"8-Maniac",
+    "name":"Maniac",
     "brand":"Conan Gray",
     "price":6.99,
     "image":"grayManiac.jpg"
   },
   {
-    "name":"9-Colors",
+    "name":"Colors",
     "brand":"Halsey",
     "price":11.99,
     "image":"halseycolors.png"
   }
 ];
+
+
